0776c13a2b074fdf5d4485eb920231cfb0028d17
reinel_vargas <reinel.vargas@correounivalle.edu.co>
2024-11-25 15:21:58 -0500

    Added the logic to each MVC architecture components

---
+++ src/main/java/com/example/fiftyclash/models/Deck.java
@@ -0,0 +1,77 @@
+package com.example.fiftyclash.models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    private ArrayList<Card> deck;
+
+    public Deck() {
+        deck = new ArrayList<>();
+    }
+
+    public void initializeDeck() {
+        String[] suits = {"diamonds", "hearts", "clubs", "spades"};
+        String[] value = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+        for (int i = 0; i < suits.length; i++) {
+            for (int j = 0; j < value.length; j++) {
+                deck.add(new Card(value[j], suits[i]));
+            }
+        }
+        Collections.shuffle(deck);
+    }
+
+    public int getCurrentPoints() {
+        int pointsAccumulator = 0;
+        if (deck != null) {
+            for (Card card : deck) {
+                switch (card.getValue()) {
+                    case "J", "Q", "K":
+                        pointsAccumulator -= card.getCardValue();
+                        break;
+                    case "A":
+                        if (pointsAccumulator + card.getCardValue() > 50) pointsAccumulator += 1;
+                        else pointsAccumulator += card.getCardValue();
+                        break;
+                    case "9":
+                        pointsAccumulator += 0;
+                        break;
+                    default:
+                        pointsAccumulator += card.getCardValue();
+                        break;
+                }
+            }
+        }
+        return pointsAccumulator;
+    }
+
+    public void printDeck () {
+        String icon = "";
+        for (int i = 0; i < deck.size(); ++i) {
+            switch (deck.get(i).getSuit()){
+                case "diamonds": icon = "♦"; break;
+                case "hearts": icon = "♥"; break;
+                case "clubs": icon = "♣"; break;
+                case "spades": icon = "♠"; break;
+            }
+            System.out.println(deck.get(i).getValue() + icon);
+        }
+    }
+
+    public void addCard(Card card) {
+        deck.add(card);
+    }
+
+    public void removeCard(int index) {
+        deck.remove(index);
+    }
+
+    public ArrayList<Card> getDeck() {
+        return deck;
+    }
+
+    public void setDeck(ArrayList<Card> deck) {
+        this.deck = deck;
+    }
+}
---
+++ src/main/java/com/example/fiftyclash/views/HelloView.java
@@ -0,0 +1,4 @@
+package com.example.fiftyclash.views;
+
+public class HelloView {
+}
