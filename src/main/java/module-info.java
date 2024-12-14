872ad40c8e52ee31e4aff81e2f285153b8d549c1
reinel_vargas <reinel.vargas@correounivalle.edu.co>
2024-12-14 18:54:28 -0500

    docs: javadoc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.example.fiftyclash {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.smartcardio;
-
+    requires org.junit.jupiter.api;
+    requires org.apiguardian.api;
 
     opens com.example.fiftyclash to javafx.fxml;
     opens com.example.fiftyclash.controllers to javafx.fxml;
--- src/test/java/com/example/fiftyclash/models/CardTest.java
+++ src/test/java/com/example/fiftyclash/models/CardTest.java
@@ -9,28 +9,32 @@ class CardTest {
     @Test
     void testGetCardValueWithNumber() {
         Card eight = new Card("8", "clubs");
+        int currentPoints = 10;
 
         int expectedValue = 8;
 
-        assertEquals(expectedValue, eight.getCardValue());
+        assertEquals(expectedValue, eight.getCardValue(currentPoints));
     }
 
     @Test
     void testGetCardValueWithLetter() {
         Card king = new Card("K", "hearts");
+        int currentPoints = 10;
 
         int expectedValue = -10;
 
-        assertEquals(expectedValue, king.getCardValue());
+        assertEquals(expectedValue, king.getCardValue(currentPoints));
     }
 
     @Test
     void testGetCardValueWithNine() {
         Card nine = new Card("9", "diamonds");
+        int currentPoints = 10;
+
 
         int expectedValue = 0;
 
-        assertEquals(expectedValue, nine.getCardValue());
+        assertEquals(expectedValue, nine.getCardValue(currentPoints));
     }
 
 }
